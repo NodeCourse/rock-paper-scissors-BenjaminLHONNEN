86ed6fb5aad7167f8f4b50943312390527f891ce
BenjaminLHONNEN <benjaminlhonnen@gmail.com>
2018-05-15 09:42:14 +0200

    mega Opti

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const randomItem = require('random-item');
+const data = require('./data.json');
+
+let dataObject = {};
+let possibleChoice = [];
+for (let p = 0; p < data.length; p++) {
+    let item = data[p];
+    possibleChoice.push(item.id);
+    dataObject[item.id] = {
+        "id": item.id,
+        "imgSrc": item.imgSrc,
+        "winAgaints": item.winAgaints,
+    };
+}
 
 // Use Pug to render views
 app.set('view engine', 'pug');
@@ -17,154 +30,41 @@ app.use(bodyParser.json());
 
 
 app.get('/', (req, res) => {
-    res.render('homepage');
+    res.render('homepage', {data: data});
 });
 
 app.get('/result/:choice', (req, res) => {
     const choice = req.params.choice;
+    if (possibleChoice.indexOf(choice) !== -1) {
+        let appChoice = randomAppChoice(choice);
 
-    let userChoice = choiceParser(choice);
-    let appChoice = randomAppChoice(choice);
+        console.log(appChoice);
 
-    let obj = {
-        result: doesFirstChoiceWin(userChoice.choice, appChoice.choice),
-        userChoice: userChoice,
-        appChoice: appChoice
-    };
-    console.log(obj);
-    res.render('result', {obj: obj});
+        let obj = {
+            result: doesFirstChoiceWin(choice, appChoice),
+            app: dataObject[appChoice],
+            user: dataObject[choice]
+        };
+        console.log(obj);
+        res.render('result', {obj: obj});
+    }
 });
 
 function doesFirstChoiceWin(choice1, choice2) {
-    if (choice1 === choice2) {
-        return "Egalité"
-    } else if (choice1 === "paper") {
-        if (doesPaperWin(choice2)) {
-            return "Gagné !"
-        } else {
-            return "Perdu !"
-        }
-    } else if (choice1 === "rock") {
-        if (doesRockWin(choice2)) {
-            return "Gagné !"
-        } else {
-            return "Perdu !"
-        }
-    } else if (choice1 === "scissors") {
-        if (doesScissorsWin(choice2)) {
-            return "Gagné !"
-        } else {
-            return "Perdu !"
-        }
-    } else if (choice1 === "spock") {
-        if (doesSpockWin(choice2)) {
-            return "Gagné !"
-        } else {
-            return "Perdu !"
-        }
-    } else if (choice1 === "lizzard") {
-        if (doesLizzardWin(choice2)) {
-            return "Gagné !"
-        } else {
-            return "Perdu !"
-        }
-    }
-}
+    console.log(dataObject);
+    console.log(dataObject[choice1]);
 
-function doesScissorsWin(compareEl) {
-    if (compareEl === "paper" || compareEl === "lizzard") {
-        return true;
+    if (dataObject[choice1]['winAgaints'].indexOf(choice2)) {
+        return "Gagné !"
+    } else if (dataObject[choice2]['winAgaints'].indexOf(choice1)) {
+        return "Perdu !"
     } else {
-        return false;
-    }
-}
-
-function doesPaperWin(compareEl) {
-    if (compareEl === "spock" || compareEl === "rock") {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function doesRockWin(compareEl) {
-    if (compareEl === "scissors" || compareEl === "lizzard") {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function doesSpockWin(compareEl) {
-    if (compareEl === "scissors" || compareEl === "rock") {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function doesLizzardWin(compareEl) {
-    if (compareEl === "spock" || compareEl === "paper") {
-        return true;
-    } else {
-        return false;
+        return "Egalité !"
     }
 }
 
 function randomAppChoice() {
-    const possibleChoice = ["paper", "scissors", "rock", "spock", "lizzard"];
-    return choiceParser(randomItem(possibleChoice));
-}
-
-function choiceParser(choice) {
-    let obj = {};
-    obj.choice = choice;
-    obj.isPaper = isPaper(choice);
-    obj.isScissors = isScissors(choice);
-    obj.isRock = isRock(choice);
-    obj.isSpock = isSpock(choice);
-    obj.isLizzard = isLizzard(choice);
-    return obj;
-}
-
-function isPaper(choice) {
-    if (choice === "paper") {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function isRock(choice) {
-    if (choice === "rock") {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function isScissors(choice) {
-    if (choice === "scissors") {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function isSpock(choice) {
-    if (choice === "spock") {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function isLizzard(choice) {
-    if (choice === "lizzard") {
-        return true;
-    } else {
-        return false;
-    }
+    return randomItem(possibleChoice);
 }
 
 app.listen(3000);
